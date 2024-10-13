5f6d65cc969a0dd50c6c32feb6cdbfe71152fb5a
sbx_user1051 <sbx_user1051@169.254.118.21>
2024-10-13 01:32:31 +0000

    Sun Oct 13 01:32:31 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -54,8 +54,7 @@ def checkout(skus):
 
     special_offers = {
         'A': [(5, 200), (3, 130)],
-        'B': (2, 45),
-        'E': (2, 40)
+        'B': (2, 45)
     }
 
     item_counts = {
@@ -79,21 +78,25 @@ def checkout(skus):
                 total += (count // offer_qty) * offer_price
                 count %= offer_qty
             total += count * prices['A']
+
         elif item == 'B':
             offer_qty, offer_price = special_offers['B']
             total += (count // offer_qty) * offer_price
             total += (count % offer_qty) * prices['B']
+
         elif item == 'E':
             total += (count // 2) * prices['E'] * 2
             total += (count % 2) * prices['E']
-            free_b_count = count // 2
-            item_counts['B'] = max(0, item_counts['B'] - free_b_count)
+            free_b_count = min(item_counts['B'], count // 2)
+            item_counts['B'] -= free_b_count
+
         else:
             total += count * prices[item]
 
     total += item_counts['B'] * prices['B']
 
     return total
+
 
 
 
